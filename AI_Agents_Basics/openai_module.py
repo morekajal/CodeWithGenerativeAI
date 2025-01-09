416fc9e42770a44d7338b3182266f83f7a7871da
Kajal More <56627613+morekajal@users.noreply.github.com>
2025-01-09 18:01:49 +0530

    how to setup openai chat completion flow

---
+++ AI_Agents_Basics/openai_module.py
@@ -0,0 +1,33 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#Create instance of OpenAI class
+# Set the API key for OpenAI
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
+def chat_generate_text(
+        prompt : str,
+        openai_api_key: str = None,
+        model : str = "gpt-3.5-turbo",
+        system_prompt: str = "You are a helpful assistant.",
+        temperature : float = 0.4,
+        max_tokens : int = 128
+        ):
+
+    messages = [
+        {"role": "system", "content": f"{system_prompt}"},
+        {"role": "user", "content": prompt},
+    ]
+
+    response = client.chat.completions.create(
+        model = model,
+        messages = messages,
+        temperature = temperature, 
+        max_tokens = max_tokens
+    )
+
+    return response.choices[0].message
