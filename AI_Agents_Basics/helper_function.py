ac82a50dde8e1ca00588866863cfdb347cd4781f
Kajal More <56627613+morekajal@users.noreply.github.com>
2025-01-09 18:03:00 +0530

    helper functions

---
+++ AI_Agents_Basics/helper_function.py
@@ -0,0 +1,11 @@
+def get_weather(city:str):
+    if city == 'Mumbai':
+        return "sunny"
+    if city == "Delhi":
+        return "cloudy"
+    if city == "Bangalore":
+        return "rainy"
+    if city == "Manali":
+        return "snow"
+    if city == "Ooty":
+        return "cold"
